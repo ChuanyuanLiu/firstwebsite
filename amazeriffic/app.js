30f045c07ac99e5b5da9e87c4544ec42f3ce095f
ChuanyuanLiu <liuchuanyuan@gmail.com>
2017-12-22 00:39:51 +1100

    fixed todo list, it is now fully functional

--- amazeriffic/app.js
+++ amazeriffic/app.js
@@ -12,29 +12,49 @@ var main = function() {
     return $content;
   }
 
+  var addcomment = function() {
+    var newtodo = $(".add input").val();
+    if (newtodo != "") {
+      ToDo.push(newtodo);
+      $(".tabs a:first-child span").trigger("click");
+    }
+  };
+
+  $(".add button").on("click", function() {
+    console.log("button pressed");
+    addcomment();
+  });
+
+  $(".add input").on("keypress", function(event) {
+    if (event.keyCode === 13) {
+      addcomment();
+    }
+  });
+
   $(".tabs a span").toArray().forEach(function(element) {
     // create a click handler for this element
     $(element).on("click", function() {
-
+      $("main .content .todo").hide();
+      $("main .content .add").hide();
       var $element = $(element);
 
       $(".tabs a span").removeClass("active");
       $element.addClass("active");
-      $("main .content").empty();
+      $("main .content .todo").empty();
 
       if ($element.parent().is(":nth-child(1)")) {
-        $("main .content").append(create_content(ToDo));
-      }
-      else if ($element.parent().is(":nth-child(2)")) {
-        $("main .content").append(create_content(Done));
-      }
-      else if ($element.parent().is(":nth-child(3)")) {
-        console.log("THIRD TAB CLICKED!");
+        $("main .content .todo").append(create_content(ToDo));
+        $("main .content .todo").fadeIn();
+      } else if ($element.parent().is(":nth-child(2)")) {
+        $("main .content .todo").append(create_content(Done));
+        $("main .content .todo").fadeIn();
+      } else if ($element.parent().is(":nth-child(3)")) {
+        $("main .content .add").fadeIn();
       }
       return false;
     });
   });
-
+  $(".tabs a:first-child span").trigger("click");
 };
 
 $(document).ready(main);
